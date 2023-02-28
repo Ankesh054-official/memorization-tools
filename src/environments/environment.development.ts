3bd22153220c808ae2e4b3b7ff6cf6b4311f2e0c
ANKESH <68959350+Ankesh054-official@users.noreply.github.com>
2023-02-28 08:25:32 +0000

    Added environment file

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    UNSPLESH_baseUrl: process.env["UNSPLESH_baseUrl"],
+    apiKey: process.env["UNSPLESH_API_ACCESS_KEY"],
+    SECRET_KEY: process.env["UNSPLESH_API_SECRET_KEY"]
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    UNSPLESH_baseUrl: process.env["UNSPLESH_baseUrl"],
+    apiKey: process.env["UNSPLESH_API_ACCESS_KEY"],
+    SECRET_KEY: process.env["UNSPLESH_API_SECRET_KEY"]
+};
